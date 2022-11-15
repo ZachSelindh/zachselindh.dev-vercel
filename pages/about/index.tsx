16fe6b19fe78888213bf4b901fc0fb37576bbe45
zachSelindh <zachselindh@gmail.com>
2022-11-15 12:26:35 -0600

    +'d about page links

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
+import type { NextPage } from 'next';
+import Button from '@mui/material/Button';
+import CardActions from '@mui/material/CardActions';
 import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
-const AboutPage = () => {
+const AboutPage: NextPage = () => {
     return (
         <>
-            <CardHeader title="About" />
-            <CardContent>
-                <Typography gutterBottom>
-                    Here is where about page data will go
-                </Typography>
-            </CardContent>
-        </>
+        <CardActions>
+            <a href="https://github.com/ZachSelindh" target="_blank" rel="noreferrer">
+                <Button startIcon={<GitHubIcon />}>Github</Button>
+            </a>
+            <a href="https://www.linkedin.com/in/zachselindh/" target="_blank" rel="noreferrer">
+                <Button startIcon={<LinkedInIcon />}>LinkedIn</Button>
+            </a>
+        </CardActions>
+        <CardHeader title="About Me" />
+        <CardContent>
+            <Typography variant='h6' gutterBottom>
+                My name is Zach Selindh and I am a web developer with years of experience building small-to-large applications to meet a huge range of business needs.
+            </Typography>
+            <Typography gutterBottom>
+                Test
+            </Typography>
+        </CardContent>
+    </>
     );
 };
 
